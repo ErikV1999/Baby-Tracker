ad59e2a4018080f833a607f04940ff4054986474
Erik Velazquez <erikvelazquez1999@gmail.com>
2021-10-04 20:52:27 -0500

    Made basic app to branch off of. Integrated Firebase Firestore Database. Added packages firebase_core, firebase_auth, firebase_firestore.

---
+++ lib/screens/tittle_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TittleScreen extends StatelessWidget {
+  const TittleScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text("HomePage"),);
+  }
+}
