72aebb28bb18992be957396ccfabece933dfd9f4
arturoroman <arturoman007@gmail.com>
2021-10-06 16:39:23 -0500

    Edited TitleScreen/added Feeding screen
    Temporarily added buttons to the title screen to press to go to our individual screens.

--- lib/screens/tittle_screen.dart
+++ lib/screens/tittle_screen.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:baby_tracker/screens/feeding.dart';
 
 class TittleScreen extends StatelessWidget {
   const TittleScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text("HomePage"),);
+    return Scaffold(
+      backgroundColor: Colors.white38,
+      appBar: AppBar(
+        title: Text('Homepage'),
+        centerTitle: true,
+          backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            ElevatedButton(
+            child: const Text('Feeding'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const Feeding()),
+              );
+            }
+            ),
+            ElevatedButton(
+                child: const Text('screen'),
+                onPressed: () {}
+            )
+          ],
+        )
+      )
+    );
   }
 }
+
+
