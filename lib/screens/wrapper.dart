23ac21c878305bee8be916b1a65178c00d6470b8
LuisMR001 <LuisRoman@my.unt.edu>
2021-10-07 13:52:38 -0500

    I finished a tutorial that had a way to log in user to firebase.
    I finished a tutorial on how to sign in and/or register a user to firebase. I plan to get  rid of code we don't need and comment on what each part does. I plan to do this before next tuesday.

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,26 @@
+import 'package:baby_tracker/screens/authenticate/authenticate.dart';
+import 'package:baby_tracker/screens/home/home.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:baby_tracker/models/myuser.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final user = Provider.of<MyUser?>(context);
+    print(user);
+
+    //return either Home or Authenticate widget
+    if (user == null)
+      {
+        return Authenticate();
+      }
+      else
+        {
+          return Home();
+        }
+  }
+}
