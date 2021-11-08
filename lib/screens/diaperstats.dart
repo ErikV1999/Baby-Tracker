af09c2c0f1692e58715d0fb6840ffd3cddaea8cb
luisR <LuisRoman@my.unt.edu>
2021-11-08 03:25:30 -0600

    Used Arturo's code to get data from database

--- lib/screens/diaperstats.dart
+++ lib/screens/diaperstats.dart
@@ -23,16 +23,76 @@ class _diaperstats extends State<diaperstats> {
     var docSnapshot = await collection.doc('doc_id').get();
     Map<String, dynamic>? data = docSnapshot.data();
   }*/
+  Future<void> generateData() async{
+    Query _sleepRef2 = FirebaseFirestore.instance.doc(widget.baby).collection('diaper change').orderBy("date", descending: true);
+    // Get docs from collection reference
+    QuerySnapshot querySnapshot = await _sleepRef2.get();
+    int count = 0;
+    double temp = 0.0;
+    int currDate = 0;
 
+    // Get data from docs and convert map to List
+    final allData = querySnapshot.docs.map((doc) => doc.data()).toList();
+
+    //print(dayArr[0]);
+
+    print(querySnapshot.docs);
+  }
   
   Widget build(BuildContext context) {
+    Query sleepRef = FirebaseFirestore.instance.doc(widget.baby).collection('diaper change').orderBy("indexDate", descending: true);
+    CollectionReference _sleepRef2 = FirebaseFirestore.instance.doc(widget.baby).collection('diaper change');
     String babyPath = widget.baby;
     return Scaffold(
       appBar: AppBar(
       title: Text("Diaper Change Graph"),
       backgroundColor: Colors.cyanAccent,
       ),
-      body: Text("Hi"),
+      body: FutureBuilder<QuerySnapshot>(
+        future: _sleepRef2.get(),
+        builder: (context, snapshot) {
+          if(snapshot.hasData) {
+            generateData();
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index){
+                Map data = snapshot.data!.docs[index].data() as Map;
+                return GestureDetector(
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "${data['Notes']}",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Text("Notes: " "${data['Notes']}, " +
+                              "date:" + "${data['date']}," +
+                              "Status:" + "${data['status']},",
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.black,
+                              )
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          }else{
+            return Center(child: Text('Loading'));
+          }
+      },
+      ),
+
     );
   }
 }
