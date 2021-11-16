89279e0887476fe8245b9a439a9638b66c6e2026
luisR <LuisRoman@my.unt.edu>
2021-11-16 11:23:24 -0600

    Graphs now shows all 3 statuses

--- lib/screens/diaperstats.dart
+++ lib/screens/diaperstats.dart
@@ -16,13 +16,33 @@ class diaperstats extends StatefulWidget {
 
 class _diaperstats extends State<diaperstats> {
   var listy = List<double>.filled(7,0.0);
+  late List<BarChartGroupData> showingBarGroups;
   @override
   void initState()
     {
       super.initState();
 
       generateData(); //gets data from database
-      generate(); // puts data in charts
+      //generate(); // puts data in charts
+      final barGroup1 = makeGroupData(0, 0, 0, 0);
+      final barGroup2 = makeGroupData(1, 0, 0, 0);
+      final barGroup3 = makeGroupData(2, 0, 0, 0);
+      final barGroup4 = makeGroupData(3, 0, 0, 0);
+      final barGroup5 = makeGroupData(4, 0, 0, 0);
+      final barGroup6 = makeGroupData(5, 0, 0, 0);
+      final barGroup7 = makeGroupData(6, 0, 0, 0);
+
+      final items = [
+        barGroup1,
+        barGroup2,
+        barGroup3,
+        barGroup4,
+        barGroup5,
+        barGroup6,
+        barGroup7,
+      ];
+      showingBarGroups = items;
+
 
     }
 
@@ -37,7 +57,7 @@ class _diaperstats extends State<diaperstats> {
         dayArrDC[i] = 0.0;
       }
 
-
+    showingBarGroups.clear();
     // Get data from docs and convert map to List
     final allData = querySnapshot.docs.map((doc) => doc.data()).toList();
     querySnapshot.docs.forEach((doc) {
@@ -46,24 +66,64 @@ class _diaperstats extends State<diaperstats> {
       diaperChange newdiaperc = new diaperChange(date, doc['status'],);
       myList.add(newdiaperc);
       });
+    double dryTally = 0;
+    double wetTally = 0;
+    double mixTally = 0;
+    print("hello");
     //checks if day is a dry day and adds to list, will change in next sprint with more columns
     for( var i = 0; i < myList.length;i++){
       //print((myList[i].dateOf).day.toString() + " and " + dayArr[i].toString());
 
       if(myList[i].statusOf == "Dry" && (i< 7)){
+        dryTally +=1;
         dayArrDC[i] += 1;
-        print(i.toString() + " is this " + myList[i].statusOf + " and " + dayArrDC[i].toString());
+        //print(i.toString() + " is this " + myList[i].statusOf + " and " + dayArrDC[i].toString());
+      }if(myList[i].statusOf == "Wet" && (i< 7)){
+        wetTally += 1;
+        //dayArrDC[i] += 1;
+        //print(i.toString() + " is this " + myList[i].statusOf + " and " + dayArrDC[i].toString());
       }
+      if(myList[i].statusOf == "Mix" && (i< 7)){
+        mixTally += 1;
+        //dayArrDC[i] += 1;
+        //print(i.toString() + " is this " + myList[i].statusOf + " and " + dayArrDC[i].toString());
+      }
+      final barGrouptemp = makeGroupData(i, dryTally, wetTally, mixTally);
+      showingBarGroups.add(barGrouptemp);
+      print("Dry, wet, mix and I" + dryTally.toString() + wetTally.toString() + mixTally.toString());
+      //reset for next loop
+      dryTally = wetTally = mixTally = 0;
     }
-    listy[0] = dayArrDC[0];
+    /*listy[0] = dayArrDC[0];
     listy[1] = dayArrDC[1];
     listy[2] = dayArrDC[2];
     listy[3] = dayArrDC[3];
     listy[4] = dayArrDC[4];
     listy[5] = dayArrDC[5];
-    listy[6] = dayArrDC[6];
+    listy[6] = dayArrDC[6];*/
     print("AFTER PRINT");
   }
+  BarChartGroupData makeGroupData(int x, double y1, double y2, double y3) {
+    double widths = 7;
+    return BarChartGroupData(barsSpace: 10, x: x, barRods: [
+      BarChartRodData(
+          y: y1,
+      colors: [Color(0xFF3E2723)] ,// other one 0xffff5182
+      width: widths,
+    ),
+    BarChartRodData(
+    y: y2,
+    colors: [Color(0xFF0D47A1)],
+      width: widths,
+    ),
+    BarChartRodData(
+        y: y3,
+        colors: [Color(0xFF1B5E20)],
+        width: widths,
+      )
+    ]);
+  }//makeGroupData
+
   Future<void> generate() async{//set up chart data
     setState(() {
       listy[0] = dayArrDC[0];
@@ -87,18 +147,20 @@ class _diaperstats extends State<diaperstats> {
         child: Container(
           padding: const EdgeInsets.all(10),
           color: Colors.blue,
-          height: 200,
+          height: 400,
           child: BarChart(
             BarChartData (
               maxY: 5,
-              barGroups: [BarChartGroupData(x: 1, barRods: [BarChartRodData(y: listy[0])]),
+              barGroups: showingBarGroups,
+              /*[BarChartGroupData(x: 1, barRods: [BarChartRodData(y: listy[0])]),
                 BarChartGroupData(x: 2, barRods: [BarChartRodData(y: listy[1])]),
                 BarChartGroupData(x: 3, barRods: [BarChartRodData(y: listy[2])]),
                 BarChartGroupData(x: 4, barRods: [BarChartRodData(y: listy[3])]),
                 BarChartGroupData(x: 5, barRods: [BarChartRodData(y: listy[4])]),
                 BarChartGroupData(x: 6, barRods: [BarChartRodData(y: listy[5])]),
-                BarChartGroupData(x: 7, barRods: [BarChartRodData(y: listy[6])]),],
+                BarChartGroupData(x: 7, barRods: [BarChartRodData(y: listy[6])]),],*/
             ),
+
           ),
         ),
       ),
