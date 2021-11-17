e5003275f9a42b6e4e23d7bcd83d1b0161bfe44b
Hunter-Thompson2 <hunterthompson2@my.unt.edu>
2021-11-17 05:00:40 -0600

    Added a graph for weight
    graph most works. just looks ugly. also theres an error popping up for the measurement input screen but it doesnt seem to actually effect anything

--- lib/screens/WeightGraphs.dart
+++ lib/screens/WeightGraphs.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class WeightGraphs extends StatefulWidget{
 
@@ -13,6 +14,114 @@ class WeightGraphs extends StatefulWidget{
 
 class _WeightGraphsState extends State<WeightGraphs> {
   Widget build(context){
-    return Text("hi");
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.doc(widget.baby).collection('weights').snapshots(),
+      builder: (context, snapshot){
+        return buildWithBaby(context, snapshot);
+      }
+    );
+  }
+  Widget buildWithBaby(context, snapshot){
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.doc(widget.baby).snapshots(),
+      builder: (context, snapshotBuilder){
+        return Padding(
+          padding: EdgeInsets.fromLTRB(0,20.0,0,0),
+          child: screen(context, snapshot, snapshotBuilder),
+        );
+
+
+      }
+    );
+  }
+  Widget screen(context,snapshot, snapshotBaby){
+
+    double dob = DateTime.now().millisecondsSinceEpoch.toDouble();
+    double mx = 0;
+    double mn = 0;
+    bool yr = false;
+    String xAxis = "Days";
+
+    if(!snapshot.hasData)
+      return Text("No Weight data");
+    if(snapshot.data.docs == null)
+      return Text("No Weight data");
+
+    if(!snapshotBaby.hasData)
+      return Text("No baby data");
+
+    try{
+      dob = snapshotBaby.data['dob'].toDouble();
+    } on StateError catch(e){
+      return Text("Malformed baby dob");
+    }
+
+    mx = DateTime.now().millisecondsSinceEpoch.toDouble() - dob;
+    mx = mx / 86400000;
+    if(mx > 360){
+      xAxis = "Years";
+      yr = true;
+      mx = mx / 365;
+    }
+
+    var dataPoints = List.generate(
+      snapshot.data.docs.length,
+        (i){
+          double weight = snapshot.data.docs[i]['weight'].toDouble();
+          double date = (snapshot.data.docs[i]['time'].millisecondsSinceEpoch.toDouble() - dob )/86400000;
+          if(yr == true)
+            date = date / 365;
+          return FlSpot(date, weight);
+        }
+
+    );
+
+
+
+    return ListView(
+      children: [
+        Container(
+          height: 200,
+          child:
+            LineChart(
+              LineChartData(
+                minX: mn,
+                maxX: mx,
+                minY: 0,
+                maxY: 120,
+
+                gridData: FlGridData(
+                  show: true,
+                  getDrawingHorizontalLine: (val) {
+                    return FlLine(
+                      strokeWidth: 1,
+                      color: Colors.black,
+                    );
+                  },
+
+                ),
+                  axisTitleData: FlAxisTitleData(
+                    leftTitle: AxisTitle(showTitle: true, titleText: 'Weight', margin: 4),
+                    bottomTitle: AxisTitle(showTitle: true, titleText: xAxis, margin: 4)
+                ),
+                borderData: FlBorderData(
+                  show: true,
+                  border: Border.all(color: Colors.black, width: 1),
+                ),
+                lineBarsData: [
+                  LineChartBarData(
+                    spots: dataPoints,
+                    isCurved: true,
+                    barWidth: 5,
+
+                  ),
+
+
+                ]
+              )
+            )
+        )
+      ]
+    );
   }
 }
